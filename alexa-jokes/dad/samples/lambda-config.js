cf813229a3a0672a5e0eb65cdec02f9726e831e5
Aaron Mulder <ammulder@alumni.princeton.edu>
2018-12-17 11:21:08 -0500

    Fix defaults

--- alexa-jokes/dad/samples/lambda-config.js
+++ alexa-jokes/dad/samples/lambda-config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  profile: 'dragonlock',
   region: 'us-east-1',
-  handler: 'index.default',
-  role: 'arn:aws:iam::238908776788:role/CombinedLambda',
+  handler: 'index.handler',
+  role: 'arn:aws:iam::...',
   timeout: 10,
   memorySize: 1024,
   publish: false,
   runtime: 'nodejs8.10',
-  functionName: 'MapCombined'
+  functionName: 'AlexaJokes'
 };
